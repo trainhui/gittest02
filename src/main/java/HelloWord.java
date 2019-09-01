931dbc1b47c2d6d437cf1ce6c603468979285cd7
trainhui <850131804@qq.com>
2019-09-01 17:48:34 +0800

    trainhui第一次提交

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -2,5 +2,8 @@ public class HelloWord {
     public static  void   main(String []args){
         System.out.println("hwehw");
         System.out.println("特st");
+        System.out.println("特st");System.out.println("特st");System.out.println("特st");System.out.println("特st");System.out.println("特st");
+
+
     }
 }
